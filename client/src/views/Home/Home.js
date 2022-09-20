b15d28a345335a698b752cfabe82d0c55a1d6460
El F <ffranbarbier@gmail.com>
2022-09-20 20:46:27 -0300

    anims

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from "framer-motion";
 
 import parse from 'html-react-parser';
+import CountUp from 'react-countup';
 
 import './Home.css';
 import Boton1 from '../../components/Boton1/Boton1';
@@ -24,6 +25,8 @@ const Home = ({ setActiveTab }) => {
   const [openCheckout, setOpenCheckout] = useState(false)
 
   const [platformData, setPlatformData] = useState({descripcion: '<div></div>'})
+
+  const [counter1, setCounter1] = useState(0)
     
 
   const [recibir, setRecibir] = useState(0)
@@ -71,9 +74,54 @@ const Home = ({ setActiveTab }) => {
   const plataformas = useSelector(state => state.plataformas)
   const faqs = useSelector(state => state.faqs) 
 
+  const textAnim = {
+    hidden: { transform: "translate3d(0, 5em, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0deg, 25deg)" },
+    show: {
+      transform: "translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0deg, 0deg)",
+    //   transition: {ease: [0.26, 1.03, 0, 1]}
+    }
+  }
 
 
-       
+  const ref = useRef()
+  const isVisible = useOnScreen(ref)
+
+  function useOnScreen(ref) {
+
+    const [isIntersecting, setIntersecting] = useState(false)
+  
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+  
+    useEffect(() => {
+      observer.observe(ref.current)
+      // Remove the observer as soon as the component is unmounted
+      return () => { observer.disconnect() }
+    }, [])
+  
+    return isIntersecting
+  }
+
+
+  function counterUno(minimum, maximum)  {
+    var cnt = minimum
+    var obj1;
+
+    setInterval(function(){
+      cnt++;
+      obj1 = cnt;
+      if(cnt == 5000){
+        setCounter1(cnt)
+        // clearInterval(timerMy);
+        return false
+      }
+
+    },0
+    );
+}
+
+
   function render(){
       return  <div id="Home-view">
           <nav id="navv">
@@ -89,15 +137,33 @@ const Home = ({ setActiveTab }) => {
           </nav>
                     <main>
                         <header>
-                            <h1>Tu exchanger de confianza.</h1>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                            <div style={{'overflow':'hidden'}}>
+                            <motion.h1
+                                className="framerAnim"
+                                variants={textAnim}
+                                initial="hidden"
+                                animate="show"
+                                transition={{ease: [0.26, 1.03, 0, 1] }}
+                            >Tu exchanger de confianza.</motion.h1>
+                            </div>
+                            <motion.p
+                             initial={{ opacity: 0}}
+                             animate={{ opacity: 1}}
+                             transition={{ delay: 2.5 }}
+                             className='framerAnim'
+                            >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</motion.p>
                             <a href="#plataformas" className="big-btn"><Boton1 text={'Encontrá tu plataforma'}/></a>
                             <a href="#faq-section" ><button>Preguntas frecuentes</button></a>
                         </header>
                         <div>
                           <div onClick={()=>{ setViewPlatforms(!viewPlatforms) }}>
                             <span>Mirá con las plataformas que trabajamos</span>
-                            <div id="mira-plataf">
+                            <motion.div id="mira-plataf"
+                              animate={{
+                                scale: [0.8, 0.9, 1, 1.05, 1],
+                                opacity: [0, 1, 1, 1, 1]
+                              }}
+                            >
                               <div >
                                 <div>
                                   <div>
@@ -138,14 +204,20 @@ const Home = ({ setActiveTab }) => {
                                   ))}
                               </ul>
                               }
-                            </div>
+                            </motion.div>
                           </div>
                         </div>
                     </main>
                 <div className='main-cont'>
                     <div id="steps">
                       <div>
-                          <h3>Obtené tu dinero en 3 simples pasos</h3>
+                          <motion.h3
+                            initial={{y: 20, opacity: 0}}
+                            whileInView={{y: 0, opacity: 1}}
+                            viewport={{ once: true }}
+                            transition={{ delay: 0.1, duration : 1 }}
+
+                          >Obtené tu dinero en 3 simples pasos</motion.h3>
                           <div id="diagrama">
                               <img src="/assets/diagram.svg" />
                           </div>
@@ -153,10 +225,21 @@ const Home = ({ setActiveTab }) => {
                     </div>
                     <div id="plataformas"></div>
                     <div id="plataformas-cont">
-                        <h3>Dónde tenés tu saldo?</h3>
+                        <motion.h3
+                            initial={{y: 20, opacity: 0}}
+                            whileInView={{y: 0, opacity: 1}}
+                            viewport={{ once: true }}
+                            transition={{ delay: 0.1, duration : 1 }}
+
+                          >Dónde tenés tu saldo?</motion.h3>
                         <ul>
                           {plataformas.map((plataforma)=>(
-                            <li
+                            <motion.li
+                              initial={{y: 20, opacity: 0}}
+                              whileInView={{y: 0, opacity: 1}}
+                              viewport={{ once: true }}
+                              transition={{ duration : 1 }}
+
                               id={plataforma.nombre}
                               className={ plataforma.nombre == platformData?.nombre && "plat-selected"} 
                               onClick={ ()=>{ 
@@ -267,28 +350,47 @@ const Home = ({ setActiveTab }) => {
 
                                 </div>
                               }
-                            </li>
+                            </motion.li>
                           )) 
                           }
                         </ul>
                     </div>
                     <div id="testimonios">
                         <div>
-                          <h3>Qué dicen nuestros clientes</h3>
+                        <motion.h3
+                            initial={{y: 20, opacity: 0}}
+                            whileInView={{y: 0, opacity: 1}}
+                            viewport={{ once: true }}
+                            transition={{ delay: 0.1, duration : 1 }}
+
+                          >Qué dicen nuestros clientes</motion.h3>
                           <div>
                             <div id="numeros">
-                              <div>
-                                <p>5.000</p>
+                              <div ref={ref}>
+                                <p>
+                                {isVisible &&
+                                  <CountUp start={0} end={5000} />
+                                }
+                                </p>
                                 <span>Clientes contentos</span>
                               </div>
                               <div>
-                                <p>300</p>
+                                <p>
+                                {isVisible &&
+                                  <CountUp start={0} end={300} />
+                                }
+                                </p>
                                 <span>Operacion diarias</span>
                               </div>
                             </div>
 
                             <div id="notifications">
-                                <div>
+                                <motion.div
+                                  initial={{x: -20, opacity: 0}}
+                                  whileInView={{x: 0, opacity: 1}}
+                                  viewport={{ once: true }}
+                                  transition={{ delay: 0.3, duration : 0.5 }}
+                                >
                                   <div>
                                     <div className='notif-img'>
                                       <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Jos%C3%A9_Larralde_Quilmes.jpg"/>
@@ -299,9 +401,14 @@ const Home = ({ setActiveTab }) => {
                                     </div>
                                     <span>2 days ago</span>
                                   </div>
-                                </div>
+                                </motion.div>
 
-                                <div>
+                                <motion.div
+                                  initial={{x: -20, opacity: 0}}
+                                  whileInView={{x: 0, opacity: 1}}
+                                  viewport={{ once: true }}
+                                  transition={{ delay: 0.5, duration : 0.5 }}
+                                >
                                   <div>
                                     <div className='notif-img'>
                                       <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Jos%C3%A9_Larralde_Quilmes.jpg"/>
@@ -312,9 +419,14 @@ const Home = ({ setActiveTab }) => {
                                     </div>
                                     <span>2 days ago</span>
                                   </div>
-                                </div>
+                                </motion.div>
 
-                                <div>
+                                <motion.div
+                                  initial={{x: -20, opacity: 0}}
+                                  whileInView={{x: 0, opacity: 1}}
+                                  viewport={{ once: true }}
+                                  transition={{ delay: 0.7, duration : 0.5 }}
+                                >
                                   <div>
                                     <div className='notif-img'>
                                       <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Jos%C3%A9_Larralde_Quilmes.jpg"/>
@@ -325,9 +437,14 @@ const Home = ({ setActiveTab }) => {
                                     </div>
                                     <span>2 days ago</span>
                                   </div>
-                                </div>
+                                </motion.div>
 
-                                <div>
+                                <motion.div
+                                  initial={{x: -20, opacity: 0}}
+                                  whileInView={{x: 0, opacity: 1}}
+                                  viewport={{ once: true }}
+                                  transition={{ delay: 0.9, duration : 0.5 }}
+                                >
                                   <div>
                                     <div className='notif-img'>
                                       <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Jos%C3%A9_Larralde_Quilmes.jpg"/>
@@ -338,16 +455,27 @@ const Home = ({ setActiveTab }) => {
                                     </div>
                                     <span>2 days ago</span>
                                   </div>
-                                </div>
+                                </motion.div>
                             </div>
                           </div>
                         </div>
                     </div>
                     <div id="faq-section">
-                        <h3>Tenés preguntas?</h3>
+                          <motion.h3
+                            initial={{y: 20, opacity: 0}}
+                            whileInView={{y: 0, opacity: 1}}
+                            viewport={{ once: true }}
+                            transition={{ delay: 0.1, duration :1 }}
+
+                          >Tenés preguntas?</motion.h3>
                         <ul>
                           {faqs.map((faq, index)=>(
-                            <li onClick={()=>{setSelectedFaq(index)}}>
+                            <motion.li onClick={()=>{setSelectedFaq(index)}}
+                                initial={{y: 20, opacity: 0}}
+                                whileInView={{y: 0, opacity: 1}}
+                                viewport={{ once: true }}
+                                transition={{ duration : 1 }}
+                            >
                               <div className="pregunta-cont">
                                 <div className="icon-preg">
                                   <div className={selectedFaq == index && "abiertus"} ></div>
@@ -364,7 +492,7 @@ const Home = ({ setActiveTab }) => {
                                       <p>{faq.rta}</p>
                                   </motion.div>
                                 }
-                            </li>
+                            </motion.li>
                           ))
 
                           }
