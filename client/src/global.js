fea39bf29bc21dfbf31393d68ff1ccb64ef251af
Francisco Barbier <fran@Franciscos-MacBook-Pro.local>
2022-12-28 17:50:03 -0300

    server endpoint

--- client/src/global.js
+++ client/src/global.js
@@ -1,2 +1,4 @@
 // export const ENDPOINT = "http://localhost:5000/";
-export const ENDPOINT = "https://prj-calc.herokuapp.com/";
+// export const ENDPOINT = "https://prj-calc.herokuapp.com/";
+export const ENDPOINT = "https://prjexch-back.vercel.app/";
+
